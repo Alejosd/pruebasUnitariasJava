b0225d3b8c942a15408a43b30610d4ff8a8689f5
Alejosd <alejosd5@gmail.com>
2016-11-13 18:30:54 -0300

    3. implementacion setup y tearDown

--- test/com/agilelife/alejosd5/MensajeTest.java
+++ test/com/agilelife/alejosd5/MensajeTest.java
@@ -2,27 +2,43 @@ package com.agilelife.alejosd5;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MensajeTest {
+	
+	private Mensaje mensaje;
+	
+	@Before
+	public void setup(){
+		System.out.println("setup");
+		mensaje = new Mensaje();
+	}
 
 	@Test
 	public void obtenerMensajeSegunParamentro() {
-		Mensaje mensaje = new Mensaje();
+		System.out.println("obtenerMensajeSegunParamentro");
 		String resultado = mensaje.obtenerMensaje("alejosd5");
 		assertEquals("Hola: alejosd5", resultado);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void validarRespuestaMensajeNull(){
-		Mensaje mensaje = new Mensaje();
+		System.out.println("validarRespuestaMensajeNull");
 		mensaje.obtenerMensaje(null);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void validarRespuestaMensajeParamentroVacio(){
-		Mensaje mensaje = new Mensaje();
+		System.out.println("validarRespuestaMensajeParamentroVacio");
 		mensaje.obtenerMensaje("");
+	}
+	
+	@After
+	public void tearDown(){
+		System.out.println("tearDown");
+		mensaje = null;
 	}
 
 }
